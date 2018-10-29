67e9409758986990d677386b82bfa628961cfa67
huaxiauluo <huaxiauluo@hotmail.com>
2018-10-29 14:48:44 +0800

    use promise

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@
  * @param     originWorkerUrl]
  * @return    promise
  */
-export const getLocalWorkerUrl = async (originWorkerUrl) => {
-    // get JavaScript code string
-    const codeString = await fetch(originWorkerUrl).then(res => res.text());
-
+export const getLocalWorkerUrl = (originWorkerUrl) => new Promise(resolve => 
+// get JavaScript code string
+fetch(originWorkerUrl)
+.then(res => res.text())
+.then(codeString => {
     // URL format: blob:http://localhost:8089/cd2930c0-f4ca-4a9f-b6b1-8242e520dd62
     const localWorkerUrl = window.URL.createObjectURL(new Blob([codeString], {
         type: 'application/javascript'
     }));
-
-    return localWorkerUrl;
-};
+    resolve(localWorkerUrl);
+}));
