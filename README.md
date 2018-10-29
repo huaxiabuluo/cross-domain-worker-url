14edea0fee0c7ca09ed73a2d15d057b73588c3e0
huaxiabuluo <huaxiabuluo@hotmail.com>
2018-10-29 12:48:34 +0800

    update

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # cross-domain-worker-url
 
-Convert a cross-domain url to an available url.
+#### Convert a cross-domain url to an available url.
 
-## Usage
+If your current domain is `http://www.a.com/`, but you want to try `const myWorker = new Worker('http://www.b.com/hello.js')`, it will throw cross-domain error.
+
+##### We can solve the problem by using `cross-domain-worker-url`
+
 ```js
 const { getLocalWorkerUrl } from 'cross-domain-worker-url';
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 /**
- * @author luxiaolong
- * @DateTime 2018-10-29
- * @param    originWorkerUrl
- * @return   Promise
+ * getLocalWorkerUrl
+ * @author    luxiaolong
+ * @DateTime  2018-10-29
+ * @param     originWorkerUrl]
+ * @return    promise
  */
 export const getLocalWorkerUrl = async (originWorkerUrl) => {
     // get JavaScript code string
     const codeString = await fetch(originWorkerUrl).then(res => res.text());
 
-    // 得到一个同源的 URL, 如果直接使用 originSassWorkerFileUrl 会引起跨域问题
     // URL format: blob:http://localhost:8089/cd2930c0-f4ca-4a9f-b6b1-8242e520dd62
     const localWorkerUrl = window.URL.createObjectURL(new Blob([codeString], {
         type: 'application/javascript'
